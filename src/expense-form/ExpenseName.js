be9169b56473085b55b2a17c984c1e3f6ffd68b3
shrki416 <coach-ahmed@bmsnj.org>
2019-11-13 22:13:02 -0500

    ExpenseName file modified

--- src/expense-form/ExpenseName.js
+++ src/expense-form/ExpenseName.js
@@ -11,7 +11,7 @@ export class ExpenseName extends Component {
 
     handleExpenseNameChange = (e) => {
         this.setState({
-            expenseName: e.target.value
+          expenseName: e.target.value
         })
     }
     
@@ -23,7 +23,7 @@ export class ExpenseName extends Component {
               <div>
                 <input
                   type="text"
-                  placeholder="Your Expense Name Goes Here"
+                  placeholder="Name Goes Here"
                   value={this.state.expenseName}
                   onChange={this.handleExpenseNameChange}>
                 </input>
