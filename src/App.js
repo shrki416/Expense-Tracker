05ea6f7fa5aa80a81746e6c75715336be6af565b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-28 20:02:51 -0400

    Conversion WIP

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseForm from "./ExpenseForm";
 import ExpenseTable from "./ExpenseTable";
 import Header from "./Header";
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
@@ -24,106 +24,106 @@ function App() {
       </div>
     </>
   );
-}
+};
 
 export default App;
 
-import React from "react";
-import ExpenseForm from "./ExpenseForm";
-import ExpenseTable from "./ExpenseTable";
-import Header from "./Header";
+// import React from "react";
+// import ExpenseForm from "./ExpenseForm";
+// import ExpenseTable from "./ExpenseTable";
+// import Header from "./Header";
 
-class App extends React.Component {
-  constructor() {
-    super();
+// class App extends React.Component {
+//   constructor() {
+//     super();
 
-    this.state = {
-      type: "",
-      name: "",
-      date: "",
-      amount: "",
-      checked: false,
-      items: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.checkedItem = this.handleCheck.bind(this);
-  }
+//     this.state = {
+//       type: "",
+//       name: "",
+//       date: "",
+//       amount: "",
+//       checked: false,
+//       items: [],
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.deleteItem = this.deleteItem.bind(this);
+//     this.checkedItem = this.handleCheck.bind(this);
+//   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+//   handleSubmit = (e) => {
+//     e.preventDefault();
 
-    let items = [...this.state.items];
+//     let items = [...this.state.items];
 
-    items.push({
-      type: this.state.type,
-      name: this.state.name,
-      date: this.state.date,
-      amount: this.state.amount,
-      checked: this.state.checked,
-    });
+//     items.push({
+//       type: this.state.type,
+//       name: this.state.name,
+//       date: this.state.date,
+//       amount: this.state.amount,
+//       checked: this.state.checked,
+//     });
 
-    this.setState({
-      items,
-      type: "cash",
-      name: "",
-      date: "",
-      amount: "",
-      checked: false,
-    });
-  };
+//     this.setState({
+//       items,
+//       type: "cash",
+//       name: "",
+//       date: "",
+//       amount: "",
+//       checked: false,
+//     });
+//   };
 
-  handleChange = (e) => {
-    const target = e.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-    this.setState({ [name]: value });
-  };
+//   handleChange = (e) => {
+//     const target = e.target;
+//     const value = target.type === "checkbox" ? target.checked : target.value;
+//     const name = target.name;
+//     this.setState({ [name]: value });
+//   };
 
-  handleCheck = (index) => {
-    const items = [...this.state.items];
-    const item = items[index];
-    item.checked = !item.checked;
-    this.setState({ items: items });
-  };
+//   handleCheck = (index) => {
+//     const items = [...this.state.items];
+//     const item = items[index];
+//     item.checked = !item.checked;
+//     this.setState({ items: items });
+//   };
 
-  deleteItem() {
-    const newItems = this.state.items.filter((item) => !item.checked);
-    this.setState({ items: newItems });
-  }
+//   deleteItem() {
+//     const newItems = this.state.items.filter((item) => !item.checked);
+//     this.setState({ items: newItems });
+//   }
 
-  componentDidMount() {
-    const items = localStorage.getItem("items") || "[]";
-    this.setState({ items: JSON.parse(items) });
-  }
+//   componentDidMount() {
+//     const items = localStorage.getItem("items") || "[]";
+//     this.setState({ items: JSON.parse(items) });
+//   }
 
-  componentDidUpdate() {
-    localStorage.setItem("items", JSON.stringify(this.state.items));
-  }
+//   componentDidUpdate() {
+//     localStorage.setItem("items", JSON.stringify(this.state.items));
+//   }
 
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="container">
-          <ExpenseForm
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-            name={this.state.name}
-            type={this.state.type}
-            date={this.state.date}
-            amount={this.state.amount}
-          />
-          <ExpenseTable
-            items={this.state.items}
-            deleteItem={this.deleteItem}
-            checkedItem={this.handleCheck}
-          />
-        </div>
-      </>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <>
+//         <Header />
+//         <div className="container">
+//           <ExpenseForm
+//             handleChange={this.handleChange}
+//             handleSubmit={this.handleSubmit}
+//             name={this.state.name}
+//             type={this.state.type}
+//             date={this.state.date}
+//             amount={this.state.amount}
+//           />
+//           <ExpenseTable
+//             items={this.state.items}
+//             deleteItem={this.deleteItem}
+//             checkedItem={this.handleCheck}
+//           />
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
