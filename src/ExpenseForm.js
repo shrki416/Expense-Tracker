db81a81358837fc0655ea8ead9db8dd9ab1903ec
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-04-15 11:47:38 -0400

    ExpenseForm Styling WIP

--- src/ExpenseForm.js
+++ src/ExpenseForm.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Button, TextField, Grid, makeStyles } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 
 const useStyles = makeStyles((theme) => ({
@@ -17,80 +23,77 @@ const ExpenseForm = ({ inputs, change, submit }) => {
   const classes = useStyles();
 
   return (
-    <Grid container justify="space-around">
-      <form onSubmit={submit} className={classes.root}>
-        <div flexDirection="row">
-          <div>
-            <h2>Expense Form</h2>
-          </div>
-          <div>
-            <div>
-              <TextField
-                id="outlined-basic"
-                label="Expense Name"
-                variant="outlined"
-                name="name"
-                type="text"
-                value={name}
-                onChange={(e) => change(e)}
-                required="required"
-                size="small"
-              />
-            </div>
-            <div>
-              <label>Payment Method: </label>
+    <form onSubmit={submit} className={classes.root}>
+      <Typography variant="h5">Expense Form</Typography>
+      <Grid container alignItems="center" justify="center">
+        <Grid item>
+          <TextField
+            id="outlined-basic"
+            label="Expense Name"
+            variant="outlined"
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => change(e)}
+            required
+            size="small"
+          />
+        </Grid>
 
-              <select
-                required="required"
-                name="type"
-                value={type}
-                onChange={(e) => change(e)}
-              >
-                <option value="cash">Cash</option>
-                <option value="check">Check</option>
-                <option value="credit-card">Credit Card</option>
-                <option value="debit-card">Debit Card</option>
-              </select>
-            </div>
-            <div>
-              <label>Transaction Date:</label>
-              <div>
-                <input
-                  required="required"
-                  name="date"
-                  type="date"
-                  value={date}
-                  onChange={(e) => change(e)}
-                />
-              </div>
-            </div>
-            <div>
-              <TextField
-                id="outlined-basic"
-                label="Expense Amount"
-                variant="outlined"
-                name="amount"
-                type="number"
-                value={amount}
-                onChange={(e) => change(e)}
-                required="required"
-                size="small"
-              />
-            </div>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              endIcon={<SendIcon />}
-            >
-              Add Expense
-            </Button>
-          </div>
-        </div>
-      </form>
-    </Grid>
+        <Grid item>
+          <label>Payment Method: </label>
+
+          <select
+            required="required"
+            name="type"
+            value={type}
+            onChange={(e) => change(e)}
+          >
+            <option value="cash">Cash</option>
+            <option value="check">Check</option>
+            <option value="credit-card">Credit Card</option>
+            <option value="debit-card">Debit Card</option>
+          </select>
+        </Grid>
+
+        <Grid item>
+          <TextField
+            variant="outlined"
+            type="date"
+            name="date"
+            value={date}
+            onChange={(e) => change(e)}
+            required
+            size="small"
+          />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            id="outlined-basic"
+            label="Expense Amount"
+            variant="outlined"
+            name="amount"
+            type="number"
+            value={amount}
+            onChange={(e) => change(e)}
+            required
+            size="small"
+          />
+        </Grid>
+
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            endIcon={<SendIcon />}
+          >
+            Add Expense
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
